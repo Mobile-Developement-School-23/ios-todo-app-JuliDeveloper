74601e8025e8bb0d4bfc9a2c2945500cdf8d191a
JuliaRomanenko <rja-97@yandex.ru>
2023-06-20 16:06:21 +0300

    HOMEWORK-02: Внесла изменения в код

---
+++ ToDoList/Presentations/TodoList/TodoListViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class TodoListViewController: UIViewController {
+    
+    private lazy var openButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .systemBlue
+        button.setTitle("Go", for: .normal)
+        button.titleLabel?.font = UIFont.tdLargeTitle
+        button.tintColor = .white
+        button.layer.cornerRadius = Constants.radius
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(openDetailVC), for: .touchUpInside)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .tdBackPrimaryColor
+        setupButton()
+    }
+    
+    private func setupButton() {
+        view.addSubview(openButton)
+        
+        NSLayoutConstraint.activate([
+            openButton.widthAnchor.constraint(equalToConstant: 200),
+            openButton.heightAnchor.constraint(equalToConstant: 50),
+            openButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            openButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+
+    @objc private func openDetailVC() {
+        let detailVC = DetailTodoItemViewController()
+        let navController = UINavigationController(rootViewController: detailVC)
+        present(navController, animated: true)
+    }
+}
