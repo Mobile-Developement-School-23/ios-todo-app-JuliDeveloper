04aa8237673f876cc5f0ddb2c45d46b2106e8bb2
JuliaRomanenko <rja-97@yandex.ru>
2023-06-27 15:22:53 +0300

    HOMEWORK-03: Добавила UIContextualActions для таблицы

--- ToDoList/Presentations/TodoList/TodoListViewController.swift
+++ ToDoList/Presentations/TodoList/TodoListViewController.swift
@@ -52,6 +52,46 @@ class TodoListViewController: UIViewController {
         navigationController?.navigationBar.layoutMargins.left = 32
         navigationController?.navigationBar.layoutMargins.right = 32
     }
+    
+    private func createIsDoneAction(at indexPath: IndexPath) -> UIContextualAction {
+        let todoItem = viewModel.todoItems[indexPath.row]
+        let action = UIContextualAction(style: .normal, title: nil) { (action, view, completion) in
+            
+            let updateTodoItem = TodoItem(id: todoItem.id,text: todoItem.text, importance: todoItem.importance, isDone: !todoItem.isDone)
+            
+            self.viewModel.addItem(updateTodoItem)
+            completion(true)
+        }
+        
+        action.backgroundColor = .tdGreenColor
+        action.image = UIImage(named: "isDoneAction")
+        return action
+    }
+    
+    private func createInfoAction(at indexPath: IndexPath) -> UIContextualAction {
+        let todoItem = viewModel.todoItems[indexPath.row]
+        let action = UIContextualAction(style: .normal, title: nil) { (action, view, completion) in
+            
+            print("Info")
+            completion(true)
+        }
+        
+        action.backgroundColor = .tdGrayLightColor
+        action.image = UIImage(named: "infoAction")
+        return action
+    }
+    
+    private func createDeleteAction(tableView: UITableView, at indexPath: IndexPath) -> UIContextualAction {
+        let todoItem = viewModel.todoItems[indexPath.row]
+        let action = UIContextualAction(style: .normal, title: nil) { (action, view, completion) in
+            self.viewModel.deleteItem(with: todoItem.id)
+            completion(true)
+        }
+        
+        action.backgroundColor = .tdRedColor
+        action.image = UIImage(named: "deleteAction")
+        return action
+    }
 }
 
 extension TodoListViewController: UITableViewDataSource {
@@ -78,7 +118,24 @@ extension TodoListViewController: UITableViewDataSource {
 }
 
 extension TodoListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        if indexPath.row == viewModel.todoItems.count {
+            return nil
+        }
+        
+        let idDoneAction = createIsDoneAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [idDoneAction])
+    }
     
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        if indexPath.row == viewModel.todoItems.count {
+            return nil
+        }
+        
+        let infoAction = createInfoAction(at: indexPath)
+        let deleteAction = createDeleteAction(tableView: tableView, at: indexPath)
+        return UISwipeActionsConfiguration(actions: [deleteAction, infoAction])
+    }
 }
 
 extension TodoListViewController: TodoListViewControllerDelegate {
