cae68bef825f325fdfd6814911a6ec3fff5ce62c
JuliaRomanenko <rja-97@yandex.ru>
2023-07-05 13:10:09 +0300

    HOMEWORK-05: Продолжаю работу над extension для URLSession

--- ToDoList/Resources/Extensions/URLSession+Extension.swift
+++ ToDoList/Resources/Extensions/URLSession+Extension.swift
@@ -1,47 +1,57 @@
-import Foundation
-
-private enum NetworkError: Error {
-    case noData
-    case codeError
-}
-
-extension URLSession {
-//    func fetchData(
-//        for urlRequest: URLRequest,
-//        completion: @escaping @Sendable (Result<(Data, URLResponse), Error>) -> Void
-//    ) -> URLSessionTask {
-//        let task = dataTask(with: urlRequest) { (data, response, error) in
-//            if let response = response as? HTTPURLResponse {
-//                if response.statusCode >= 200 && response.statusCode < 300 {
-//                    if let data = data {
-//                        completion(.success((data, response)))
-//                    } else {
-//                        completion(.failure(NetworkError.noData))
-//                    }
-//                } else if let error = error {
-//                    completion(.failure(error))
-//                } else {
-//                    completion(.failure(NetworkError.codeError))
-//                }
-//            }
-//        }
-//        task.resume()
+//import Foundation
 //
-//        return task
+//private enum NetworkError: Error {
+//    case noData
+//    case codeError
+//}
+//
+//extension URLSession {
+//    func fetchData(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+//        //        return try await withCheckedThrowingContinuation { continuation in
+//        //            let task = self.dataTask(with: urlRequest) { (data, response, error) in
+//        //                if let response = response as? HTTPURLResponse {
+//        //                    if response.statusCode >= 200 && response.statusCode < 300 {
+//        //                        if let data = data {
+//        //                            continuation.resume(returning: (data, response))
+//        //                        } else {
+//        //                            continuation.resume(throwing: NetworkError.noData)
+//        //                            continuation.resume(throwing: NetworkError.codeError)
+//        //                        }
+//        //                    }
+//        //                } else if let error = error {
+//        //                    continuation.resume(throwing: error)
+//        //                }
+//        //            }
+//        //            task.resume()
+//        //        }
+//
+//        //        return try await withTaskCancellationHandler {
+//        //            var task: URLSessionDataTask?
+//        //            try await withCheckedThrowingContinuation { continuation in
+//        //                task = self.dataTask(with: urlRequest) { (data, response, error) in
+//        //                    if let error = error {
+//        //                        continuation.resume(throwing: error)
+//        //                    } else if let response = response as? HTTPURLResponse, let data = data {
+//        //                        if (200...299).contains(response.statusCode) {
+//        //                            continuation.resume(returning: (data, response))
+//        //                        } else {
+//        //                            continuation.resume(throwing: NetworkError.codeError)
+//        //                        }
+//        //                    } else {
+//        //                        continuation.resume(throwing: NetworkError.noData)
+//        //                    }
+//        //                }
+//        //                task.resume()
+//        //            } onCancel: {
+//        //                await Task.sleep(for: .nanoseconds(1))
+//        //                if Task.isCancelled {
+//        //                    task?.cancel()
+//        //                }
+//        //            }
+//        //            //task.resume()
+//        //        }
+//        //    }
+//
+//        //}
 //    }
-    
-        func fetchData(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
-            return try await withCheckedThrowingContinuation { continuation in
-                let task = self.dataTask(with: urlRequest) { (data, response, error) in
-                if let error = error {
-                    continuation.resume(throwing: error)
-                } else if let data = data, let response = response {
-                    continuation.resume(returning: (data, response))
-                } else {
-                    continuation.resume(throwing: NetworkError.noData)
-                }
-            }
-            task.resume()
-        }
-    }
-}
+//}
