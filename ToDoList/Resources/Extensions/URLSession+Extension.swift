56a56e31e55fafb9377943d81a5f843982d34879
JuliaRomanenko <rja-97@yandex.ru>
2023-07-06 14:06:05 +0300

    HOMEWORK-05: Доработала extension для URLSession

--- ToDoList/Resources/Extensions/URLSession+Extension.swift
+++ ToDoList/Resources/Extensions/URLSession+Extension.swift
@@ -1,57 +1,32 @@
-//import Foundation
-//
-//private enum NetworkError: Error {
-//    case noData
-//    case codeError
-//}
-//
-//extension URLSession {
-//    func fetchData(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
-//        //        return try await withCheckedThrowingContinuation { continuation in
-//        //            let task = self.dataTask(with: urlRequest) { (data, response, error) in
-//        //                if let response = response as? HTTPURLResponse {
-//        //                    if response.statusCode >= 200 && response.statusCode < 300 {
-//        //                        if let data = data {
-//        //                            continuation.resume(returning: (data, response))
-//        //                        } else {
-//        //                            continuation.resume(throwing: NetworkError.noData)
-//        //                            continuation.resume(throwing: NetworkError.codeError)
-//        //                        }
-//        //                    }
-//        //                } else if let error = error {
-//        //                    continuation.resume(throwing: error)
-//        //                }
-//        //            }
-//        //            task.resume()
-//        //        }
-//
-//        //        return try await withTaskCancellationHandler {
-//        //            var task: URLSessionDataTask?
-//        //            try await withCheckedThrowingContinuation { continuation in
-//        //                task = self.dataTask(with: urlRequest) { (data, response, error) in
-//        //                    if let error = error {
-//        //                        continuation.resume(throwing: error)
-//        //                    } else if let response = response as? HTTPURLResponse, let data = data {
-//        //                        if (200...299).contains(response.statusCode) {
-//        //                            continuation.resume(returning: (data, response))
-//        //                        } else {
-//        //                            continuation.resume(throwing: NetworkError.codeError)
-//        //                        }
-//        //                    } else {
-//        //                        continuation.resume(throwing: NetworkError.noData)
-//        //                    }
-//        //                }
-//        //                task.resume()
-//        //            } onCancel: {
-//        //                await Task.sleep(for: .nanoseconds(1))
-//        //                if Task.isCancelled {
-//        //                    task?.cancel()
-//        //                }
-//        //            }
-//        //            //task.resume()
-//        //        }
-//        //    }
-//
-//        //}
-//    }
-//}
+import Foundation
+
+private enum NetworkError: Error {
+    case noData
+    case codeError
+}
+
+extension URLSession {
+    func fetchData(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+        var task: URLSessionDataTask?
+        return try await withTaskCancellationHandler {
+            try await withCheckedThrowingContinuation { continuation in
+                task = self.dataTask(with: urlRequest) { (data, response, error) in
+                    if let response = response as? HTTPURLResponse, let data = data {
+                        if (200...299).contains(response.statusCode) {
+                            continuation.resume(returning: (data, response))
+                        } else if let error = error {
+                            continuation.resume(throwing: error)
+                        } else {
+                            continuation.resume(throwing: NetworkError.codeError)
+                        }
+                    } else {
+                        continuation.resume(throwing: NetworkError.noData)
+                    }
+                }
+                task?.resume()
+            }
+        } onCancel: { [weak task] in
+            task?.cancel()
+        }
+    }
+}
