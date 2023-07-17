2fc42b5304dbc0b966ba95ded1245f731cc55f19
JuliaRomanenko <rja-97@yandex.ru>
2023-07-17 16:03:27 +0300

    HOMEWORK-08: Добавила таргет для SwiftUI

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Julia Romanenko on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
