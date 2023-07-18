1d214b32545da9be47dd66c867142f92da74ba3e
JuliaRomanenko <rja-97@yandex.ru>
2023-07-18 15:35:38 +0300

    HOMEWORK-08: Добавила ObservableObject для item и list

--- ToDoListSwiftUI/Service/DataManager.swift
+++ ToDoListSwiftUI/Service/DataManager.swift
@@ -4,15 +4,15 @@ final class DataManager {
     static let shared = DataManager()
     
     let mockTodoItems = [
-        TodoItem(id: UUID(), text: "Полить цветы", importance: .important, deadline: Date(), isDone: false),
+        TodoItem(id: UUID(), text: "Полить цветы", importance: .important, deadline: Date(), isDone: true),
         TodoItem(id: UUID(), text: "Выгулять собаку", importance: .normal, deadline: Date().addingTimeInterval(86400), isDone: false),
-        TodoItem(id: UUID(), text: "Купить молоко", importance: .unimportant, deadline: nil, isDone: false),
+        TodoItem(id: UUID(), text: "Купить молоко", importance: .unimportant, deadline: nil, isDone: true),
         TodoItem(id: UUID(), text: "Прочитать книгу", importance: .important, deadline: Date().addingTimeInterval(3*86400), isDone: false),
         TodoItem(id: UUID(), text: "Позвонить другу", importance: .unimportant, deadline: nil, isDone: true),
         TodoItem(id: UUID(), text: "Заказать пиццу", importance: .normal, deadline: Date(), isDone: false),
         TodoItem(id: UUID(), text: "Подготовить отчет", importance: .important, deadline: Date().addingTimeInterval(2*86400), isDone: false),
         TodoItem(id: UUID(), text: "Сходить в зал", importance: .normal, deadline: nil, isDone: true),
-        TodoItem(id: UUID(), text: "Заплатить за интернет", importance: .unimportant, deadline: Date().addingTimeInterval(5*86400), isDone: false),
+        TodoItem(id: UUID(), text: "Заплатить за интернет", importance: .unimportant, deadline: Date().addingTimeInterval(5*86400), isDone: true),
         TodoItem(id: UUID(), text: "Посетить врача", importance: .important, deadline: Date().addingTimeInterval(4*86400), isDone: false)
     ]
     
